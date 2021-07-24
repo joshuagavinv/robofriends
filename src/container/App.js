f24a09c018047780f271e35981007bb6c857061b
Joshua Gavin Valerie <joshua.gavin.valerie@gmail.com>
2021-07-24 22:26:36 +1000

    add redux

--- src/container/App.js
+++ src/container/App.js
@@ -1,38 +1,46 @@
 import './App.css';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { CardList } from '../components/Cardlist';
 import { SearchBox } from '../components/SearchBox';
 import { Scroll } from '../components/Scroll';
+import { requestRobots, setSearchField } from '../actions';
 
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots : state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
+  }
+}
 
+const mapDispatchToProps = dispatch => {
+  return {
+    onSearchChange: (event) => dispatch (setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      robots: [],
-      search: ''
-    }
-  }
-
-  onSearch = (event) => {
-    this.setState({search: event.target.value})
   }
 
   render() {
-    const {robots, search} = this.state
+    const { searchField, onSearchChange, robots, isPending } = this.props
     const filteredRobots = robots.filter(robot => {
       const robotName = robot.name.toLowerCase()
-      const searchName = search.toLowerCase()
+      const searchName = searchField.toLowerCase()
       return robotName.includes(searchName)
     })
 
-    if(!robots.length) {
+    if(isPending) {
       return <h1>Loading...</h1>
     } else {
       return (
         <div className="tc">
           <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearch}/>
+          <SearchBox searchChange={onSearchChange}/>
           <Scroll>
             <CardList robots={filteredRobots}/>
           </Scroll>
@@ -42,10 +50,8 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
-    .then(users => this.setState({robots : users}))
+    this.props.onRequestRobots();
   }
 }
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App);
